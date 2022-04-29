f107483e826ecb8fa8edce426e5c24ba016e0b3b
Hamin Park <haminjp@naver.com>
2022-04-29 20:57:25 +0900

    [추가] 테스트 셋 추가

--- src/data/boardData.ts
+++ src/data/boardData.ts
@@ -1,6 +1,7 @@
 import { Board } from '../interfaces/interfaces';
 
 export const TOTAL_CELL_COUNT = 64;
+
 export const MINE_COUNT = 30;
 export const MINE_BOARD_ORIGINAL: Board = [
   ['none', 'mine', 'none', 'mine', 'mine', 'none', 'none', 'none'],
@@ -13,6 +14,28 @@ export const MINE_BOARD_ORIGINAL: Board = [
   ['none', 'none', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine'],
 ];
 
-// MINE_BOARD.forEach((row) => {
-//   row.forEach((value) => value === 'mine' && console.count(value));
-// });
+// 테스트 셋 1
+// export const MINE_COUNT = 61;
+// export const MINE_BOARD_ORIGINAL: Board = [
+//   ['none', 'none', 'none', 'mine', 'mine', 'mine', 'mine', 'mine'],
+//   ['mine', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine'],
+//   ['mine', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine'],
+//   ['mine', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine'],
+//   ['mine', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine'],
+//   ['mine', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine'],
+//   ['mine', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine'],
+//   ['mine', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine'],
+// ];
+
+// 테스트 셋 2
+// export const MINE_COUNT = 3;
+// export const MINE_BOARD_ORIGINAL: Board = [
+//   ['mine', 'mine', 'mine', 'none', 'none', 'none', 'none', 'none'],
+//   ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
+//   ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
+//   ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
+//   ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
+//   ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
+//   ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
+//   ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
+// ];
