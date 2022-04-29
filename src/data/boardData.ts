3b3a7130f4ad55f1f959f7de31568bc236e11811
Hamin Park <haminjp@naver.com>
2022-04-29 14:22:59 +0900

    [수정] 지뢰 데이터 설정

--- src/data/boardData.ts
+++ src/data/boardData.ts
@@ -1,12 +1,17 @@
 import { CellState } from '../interfaces/interfaces';
 
+export const MINE_COUNT = 30;
 export const MINE_BOARD: CellState[][] = [
-  ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
-  ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
-  ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
-  ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
-  ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
-  ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
-  ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
-  ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
+  ['none', 'mine', 'none', 'mine', 'mine', 'none', 'none', 'none'],
+  ['none', 'none', 'none', 'none', 'none', 'mine', 'none', 'none'],
+  ['mine', 'none', 'mine', 'none', 'none', 'mine', 'none', 'mine'],
+  ['none', 'mine', 'none', 'mine', 'mine', 'none', 'mine', 'none'],
+  ['mine', 'none', 'mine', 'none', 'none', 'none', 'none', 'mine'],
+  ['mine', 'mine', 'none', 'none', 'none', 'mine', 'mine', 'mine'],
+  ['none', 'mine', 'none', 'mine', 'mine', 'none', 'mine', 'none'],
+  ['none', 'none', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine'],
 ];
+
+// MINE_BOARD.forEach((row) => {
+//   row.forEach((value) => value === 'mine' && console.count(value));
+// });
