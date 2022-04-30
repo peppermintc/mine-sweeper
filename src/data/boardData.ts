14e06300470080f15e092da0713f82a60f2e21e6
Hamin Park <haminjp@naver.com>
2022-04-30 14:55:00 +0900

    [추가] 게임 데이터 추가

--- src/data/boardData.ts
+++ src/data/boardData.ts
@@ -2,17 +2,31 @@ import { Board } from '../interfaces';
 
 export const TOTAL_CELL_COUNT = 64;
 
-export const MINE_COUNT = 30;
+// 쉬운 모드이지만 2-3분 정도 소요 예상
+export const MINE_COUNT = 8;
 export const MINE_BOARD_ORIGINAL: Board = [
-  ['none', 'mine', 'none', 'mine', 'mine', 'none', 'none', 'none'],
-  ['none', 'none', 'none', 'none', 'none', 'mine', 'none', 'none'],
-  ['mine', 'none', 'mine', 'none', 'none', 'mine', 'none', 'mine'],
-  ['none', 'mine', 'none', 'mine', 'mine', 'none', 'mine', 'none'],
-  ['mine', 'none', 'mine', 'none', 'none', 'none', 'none', 'mine'],
-  ['mine', 'mine', 'none', 'none', 'none', 'mine', 'mine', 'mine'],
-  ['none', 'mine', 'none', 'mine', 'mine', 'none', 'mine', 'none'],
-  ['none', 'none', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine'],
+  ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
+  ['none', 'none', 'none', 'none', 'none', 'none', 'mine', 'none'],
+  ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
+  ['none', 'none', 'mine', 'none', 'none', 'mine', 'none', 'none'],
+  ['none', 'mine', 'none', 'none', 'none', 'none', 'mine', 'none'],
+  ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
+  ['none', 'none', 'none', 'none', 'none', 'mine', 'mine', 'none'],
+  ['none', 'mine', 'none', 'none', 'none', 'none', 'none', 'none'],
 ];
+
+// 어려움
+// export const MINE_COUNT = 30;
+// export const MINE_BOARD_ORIGINAL: Board = [
+//   ['none', 'mine', 'none', 'mine', 'mine', 'none', 'none', 'none'],
+//   ['none', 'none', 'none', 'none', 'none', 'mine', 'none', 'none'],
+//   ['mine', 'none', 'mine', 'none', 'none', 'mine', 'none', 'mine'],
+//   ['none', 'mine', 'none', 'mine', 'mine', 'none', 'mine', 'none'],
+//   ['mine', 'none', 'mine', 'none', 'none', 'none', 'none', 'mine'],
+//   ['mine', 'mine', 'none', 'none', 'none', 'mine', 'mine', 'mine'],
+//   ['none', 'mine', 'none', 'mine', 'mine', 'none', 'mine', 'none'],
+//   ['none', 'none', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine'],
+// ];
 
 // 테스트 셋 1
 // export const MINE_COUNT = 61;
