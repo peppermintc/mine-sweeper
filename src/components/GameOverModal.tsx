46744a5fa23a8ab6e49da9b09071c167b1155924
Hamin Park <haminjp@naver.com>
2022-04-30 14:54:42 +0900

    [추가] 게임 오버 모달 이미지 추가

--- src/components/GameOverModal.tsx
+++ src/components/GameOverModal.tsx
@@ -1,5 +1,6 @@
 import { GameState } from '../interfaces';
 import ModalContainer from './ModalContainer';
+import ExplosionImage from '../img/explosion.png';
 
 interface GameOverModalProps {
   updateGameState: (newState: GameState) => void;
@@ -12,7 +13,9 @@ const GameOverModal = ({ updateGameState }: GameOverModalProps) => {
 
   return (
     <ModalContainer>
-      <h3>지뢰입니다 ㅜㅜ</h3>
+      <h3>
+        <img src={ExplosionImage} alt="explosion" /> 지뢰입니다
+      </h3>
       <h3>다시 시작 가능합니다!</h3>
       <button onClick={onRestartButtonClick}>다시 도전하기</button>
     </ModalContainer>
