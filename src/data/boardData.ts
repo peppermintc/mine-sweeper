868bc95a0e5f74acee96304156e5329fbfb712e3
Hamin Park <haminjp@naver.com>
2022-04-30 20:17:45 +0900

    [수정] 데이터 파일 수정

--- src/data/boardData.ts
+++ src/data/boardData.ts
@@ -15,20 +15,7 @@ export const MINE_BOARD_ORIGINAL: Board = [
   ['none', 'mine', 'none', 'none', 'none', 'none', 'none', 'none'],
 ];
 
-// 어려움
-// export const MINE_COUNT = 30;
-// export const MINE_BOARD_ORIGINAL: Board = [
-//   ['none', 'mine', 'none', 'mine', 'mine', 'none', 'none', 'none'],
-//   ['none', 'none', 'none', 'none', 'none', 'mine', 'none', 'none'],
-//   ['mine', 'none', 'mine', 'none', 'none', 'mine', 'none', 'mine'],
-//   ['none', 'mine', 'none', 'mine', 'mine', 'none', 'mine', 'none'],
-//   ['mine', 'none', 'mine', 'none', 'none', 'none', 'none', 'mine'],
-//   ['mine', 'mine', 'none', 'none', 'none', 'mine', 'mine', 'mine'],
-//   ['none', 'mine', 'none', 'mine', 'mine', 'none', 'mine', 'none'],
-//   ['none', 'none', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine'],
-// ];
-
-// 테스트 셋 1
+// // 테스트 셋 1
 // export const MINE_COUNT = 61;
 // export const MINE_BOARD_ORIGINAL: Board = [
 //   ['none', 'none', 'none', 'mine', 'mine', 'mine', 'mine', 'mine'],
@@ -52,4 +39,17 @@ export const MINE_BOARD_ORIGINAL: Board = [
 //   ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
 //   ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
 //   ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
+// ];
+
+// 어려움
+// export const MINE_COUNT = 30;
+// export const MINE_BOARD_ORIGINAL: Board = [
+//   ['none', 'mine', 'none', 'mine', 'mine', 'none', 'none', 'none'],
+//   ['none', 'none', 'none', 'none', 'none', 'mine', 'none', 'none'],
+//   ['mine', 'none', 'mine', 'none', 'none', 'mine', 'none', 'mine'],
+//   ['none', 'mine', 'none', 'mine', 'mine', 'none', 'mine', 'none'],
+//   ['mine', 'none', 'mine', 'none', 'none', 'none', 'none', 'mine'],
+//   ['mine', 'mine', 'none', 'none', 'none', 'mine', 'mine', 'mine'],
+//   ['none', 'mine', 'none', 'mine', 'mine', 'none', 'mine', 'none'],
+//   ['none', 'none', 'mine', 'mine', 'mine', 'mine', 'mine', 'mine'],
 // ];
